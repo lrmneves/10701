5264b0f7f9b95fd5cfe2c6fd9475d22927b411a4
Leonardo <lrmneves@poli.ufrj.br>
2015-12-01 11:20:12 -0500

    lstm without cv2

--- rnn/lstm.py
+++ rnn/lstm.py
@@ -1,8 +1,8 @@
-import numpy as np, re, theanets
+import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 from lasagne.nonlinearities import ScaledTanH
-import cv2
+# import cv2
 import numpy as np
 import theano
 import theano.tensor as T
@@ -35,17 +35,17 @@ X,Y = np.load("X.npy"),np.load("Y.npy")
 
 # Min/max sequence length
 MIN_LENGTH = 10
-MAX_LENGTH = 1
+MAX_LENGTH = 2
 # Number of units in the hidden (recurrent) layer
-N_HIDDEN = 128
+N_HIDDEN = 2048
 # Number of training sequences in each batch
-N_BATCH = 10
+N_BATCH = 4
 # Optimization learning rate
-LEARNING_RATE = 0.001
+LEARNING_RATE = 1e-2
 # All gradients above this will be clipped
-GRAD_CLIP = 100
+GRAD_CLIP = 1e2
 # How often should we check the output?
-EPOCH_SIZE = 100
+EPOCH_SIZE = 10
 # Number of epochs to train the net
 NUM_EPOCHS = 100
 
@@ -95,19 +95,23 @@ X_test = []
 Y_test = []
 
 for b in range(N_BATCH):
-	X_train.append([])
-	
-	X_test.append([])
+    X_train.append([])
 
-	for l in range(MAX_LENGTH):
-	
-		X_train[b].append(X[b*MAX_LENGTH + l])
-		Y_train.append(Y[b*MAX_LENGTH + l])
-		try:
-			X_test[b].append(X[b*MAX_LENGTH + l+9])
-			Y_test.append(Y[b*MAX_LENGTH + l+9])
-		except Exception:
-			continue
+    X_test.append([])
+    # Y_train.append(Y[b*MAX_LENGTH + MAX_LENGTH-1])
+    # Y_test.append(Y[b*MAX_LENGTH + MAX_LENGTH +9-1])
+    for l in range(MAX_LENGTH):
+
+    	X_train[b].append(X[b*MAX_LENGTH + l])
+    	
+    	try:
+    		X_test[b].append(X[b*MAX_LENGTH + l+MAX_LENGTH*N_BATCH])
+    		
+    	except Exception:
+    		continue
+    Y_train.append(Y[b*MAX_LENGTH + MAX_LENGTH-1])
+    Y_test.append(Y[b*MAX_LENGTH + MAX_LENGTH-1+MAX_LENGTH*N_BATCH])
+   
 # X_test = np.array([X[11:]])
 # Y_test = np.array([Y[11:]])
 mask_train = np.zeros((N_BATCH, MAX_LENGTH))
@@ -122,14 +126,19 @@ Y_train = np.array(Y_train)
 
 X_test= np.array(X_test)
 Y_test = np.array(Y_test)
-print len(Y_train)
-print len(Y_train[0])
+# print len(Y_train)
+# print len(Y_train[0])
+# print len(X_train)
+# print len(X_train[0])
 # print len(Y_train[0][0])
 
 # First, we build the network, starting with an input layer
 # Recurrent layers expect input of shape
 # (batch size, max sequence length, number of features)
-l_in = lasagne.layers.InputLayer(shape=(N_BATCH, MAX_LENGTH, NUM_FEATURES))
+l_in = lasagne.layers.InputLayer(shape=(None, MAX_LENGTH, NUM_FEATURES))
+
+# l_dropout = lasagne.layers.DropoutLayer(l_in,p=0.9)
+
 # The network also needs a way to provide a mask for each sequence.  We'll
 # use a separate input layer for that.  Since the mask only determines
 # which indices are part of the sequence for each batch entry, they are
@@ -141,26 +150,32 @@ l_in = lasagne.layers.InputLayer(shape=(N_BATCH, MAX_LENGTH, NUM_FEATURES))
 # for the final time step, which is all we need for this task
 l_forward_1 = lasagne.layers.LSTMLayer(
     l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,
-    nonlinearity=lasagne.nonlinearities.sigmoid)
+    nonlinearity=lasagne.nonlinearities.softmax, learn_init=True,)
+
 
 l_forward_2 = lasagne.layers.LSTMLayer(
-        l_forward_1, N_HIDDEN, grad_clipping=GRAD_CLIP,
-        nonlinearity=lasagne.nonlinearities.leaky_rectify)
+        lasagne.layers.dropout(l_forward_1, p=.2), N_HIDDEN, grad_clipping=GRAD_CLIP,
+        nonlinearity=lasagne.nonlinearities.leaky_rectify,learn_init=True)
+# l_backward_2 = lasagne.layers.LSTMLayer(
+#         lasagne.layers.dropout(l_forward_1, p=.2), N_HIDDEN, grad_clipping=GRAD_CLIP,
+#         nonlinearity=lasagne.nonlinearities.leaky_rectify,learn_init=True,backwards = True)
 
+# l_concat = lasagne.layers.ConcatLayer([l_forward_2, l_backward_2])
 l_forward_slice = lasagne.layers.SliceLayer(l_forward_2, -1, 1)
 
-# Our output layer is a simple dense connection, with 1 output unit
-l_out = lasagne.layers.DenseLayer(l_forward_slice, num_units=NUM_FEATURES, W = lasagne.init.Normal(), 
+l_out = lasagne.layers.DenseLayer(l_forward_slice, num_units=NUM_FEATURES, W = lasagne.init.GlorotNormal(), 
     nonlinearity=lasagne.nonlinearities.linear)
 
 target_values = T.matrix('target_output')
 
 # lasagne.layers.get_output produces a variable for the output of the net
 
-network_output = lasagne.layers.get_output(l_out)
-predictions = network_output
-cost = lasagne.objectives.squared_error(255*predictions, target_values).mean()
-#ost = T.mean(1 - T.batched_dot(255*predictions,target_values)/(tnorm(255*predictions)*tnorm(target_values)))
+network_output = lasagne.layers.get_output(l_out,deterministic = False)
+predictions = 255*network_output
+
+l1_reg = lasagne.regularization.regularize_layer_params(l_in,lasagne.regularization.l1)*1e-5
+# cost = lasagne.objectives.squared_error(predictions, target_values).mean() + l1_reg
+cost = T.mean(1 - T.batched_dot(predictions,target_values)/(tnorm(predictions)*tnorm(target_values)))
 # print cost
 # Retrieve all parameters from the network
 all_params = lasagne.layers.get_all_params(l_out)
@@ -173,26 +188,30 @@ train = theano.function([l_in.input_var, target_values], cost, updates=updates, 
 compute_cost = theano.function([l_in.input_var, target_values], cost, allow_input_downcast=True)
 
 probs = theano.function([l_in.input_var],network_output,allow_input_downcast=True)
+pred_func = theano.function([l_in.input_var],lasagne.layers.get_output(l_out,deterministic = True),allow_input_downcast=True)
 
 
-
- # print("Training ...")
+print("Training ...")
 try:
     for epoch in range(NUM_EPOCHS):
         for _ in range(EPOCH_SIZE):
             
             train(X_train, Y_train)
-        cost_val = compute_cost(X_train, Y_train)
-        pred = probs(np.array([X_test[0]]))
+        cost_val = compute_cost(np.array([X_train[-1]]), np.array([Y_train[-1]]))
+        
+        pred = pred_func(np.array([X_train[-1]]))
         pred = np.array(255*pred,dtype = "uint8")
+        pred = pred.reshape(64,64)
 
-        # cv2.imshow("pred",(255*pred).reshape(64,64).astype("uint8"))
-        # cv2.waitKey(200)
-        # cv2.imshow("pred",X_test[1][0].reshape(64,64).astype("uint8"))
-        # cv2.waitKey(200)
+     
+
+        # cv2.imshow("pred",pred)
+        # cv2.waitKey(1000)
+        # cv2.imshow("pred",Y_train[-1].reshape(64,64).astype("uint8"))
+        # cv2.waitKey(500)
 
         
-        if cost_val < 0.0001:
+        if cost_val < 1e-2:
             break;
         
         
@@ -212,12 +231,41 @@ except KeyboardInterrupt:
 # pred = net.predict(X[size:])
 # cv2.imshow("y",X[1].reshape(64,64).astype("uint8"))
 # cv2.waitKey(1000)
-pred = probs(X_test)
-pred = np.array(pred,dtype = "int32")
-print len(pred)
-print pred
+# pred = probs(X_test)
+# pred = np.array(pred,dtype = "int32")
+
 count = 0.0
 size = 11
+
+start_size = 10
+
+sequence = [X[i] for i in range(start_size)]
+idx_sequence = [range(start_size)]
+frames_left = set()
+
+for i in range(start_size, len(X)):
+    frames_left.add(i)
+
+while len(frames_left) > 0:
+    next_frame = pred_func(np.array([sequence]))
+
+    next_frame = np.array(255*next_frame,dtype = "uint8")
+    # cv2.imshow("pred",(next_frame).reshape(64,64).astype("uint8"))
+    # cv2.waitKey(1000)
+    similarities = [(cosine_similarity(next_frame,Y[j]),j) for j in range(start_size,len(Y))]
+    similarities = sorted(similarities)
+    similarities= similarities[::-1]
+
+    for s in similarities:
+        if s[1] in frames_left:
+            next_frame = Y[s[1]]
+            frames_left.remove(s[1])
+            idx_sequence[0].append(s[1])
+            break 
+
+    sequence.append(next_frame)
+
+print idx_sequence
 
 
 for i in range(len(X_train)):
@@ -225,8 +273,8 @@ for i in range(len(X_train)):
     pred = probs(np.array([X_train[i]]))
     pred = np.array(255*pred,dtype = "uint8")
 
-    cv2.imwrite("pred"+str(i)+".jpg",pred.reshape(64,64).astype("uint8"))
-    cv2.imwrite("y"+str(i)+".jpg",truth.reshape(64,64).astype("uint8"))
+    # cv2.imwrite("pred"+str(i)+".jpg",pred.reshape(64,64).astype("uint8"))
+    # cv2.imwrite("y"+str(i)+".jpg",truth.reshape(64,64).astype("uint8"))
 
 
     similarities = [(cosine_similarity(pred,Y_train[j]),j) for j in range(len(Y_train))]
@@ -240,9 +288,9 @@ for i in range(len(X_train)):
     else:
         if similarities[1][1]== i+1:
             count+=1
-    l = [s[1] for s in similarities[:3]]
+    l = [s[1] for s in similarities]
 
-    print i+size , l
+    print i , l
 
 
 
@@ -251,8 +299,8 @@ for i in range(len(X_test)):
     pred = probs(np.array([X_test[i]]))
     pred = np.array(255*pred,dtype = "uint8")
 
-    cv2.imwrite("pred"+str(i)+".jpg",pred.reshape(64,64).astype("uint8"))
-    cv2.imwrite("y"+str(i)+".jpg",truth.reshape(64,64).astype("uint8"))
+    # cv2.imwrite("pred"+str(i)+".jpg",pred.reshape(64,64).astype("uint8"))
+    # cv2.imwrite("y"+str(i)+".jpg",truth.reshape(64,64).astype("uint8"))
 
 
     similarities = [(cosine_similarity(pred,Y_test[j]),j+size) for j in range(len(Y_test))]
@@ -270,7 +318,7 @@ for i in range(len(X_test)):
 
     print i+size , l
 
-# print count/(20-size-1)
+print count/(20-1)
 
 # chars = re.sub(r'\s+', ' ', open('corpus.txt').read().lower())
 # txt = theanets.recurrent.Text(chars, min_count=10)
